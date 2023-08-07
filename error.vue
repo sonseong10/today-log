03e24883bacb365d30b05eb6cde39cc0f58513d9
sonseongyeol <sonseong10@gmail.com>
2023-08-07 22:32:49 +0900

    🚧 nuxt 404 페이징 작업

---
+++ error.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps(['error'])
+const handleError = () => clearError({ redirect: '/' })
+
+
+</script>
+
+<template>
+  <div class="mx-auto container">
+    <p v-html="error.statusCode"></p>
+    <p v-html="error.message"></p>
+    <button @click="handleError">Clear error</button>
+  </div>
+</template>
